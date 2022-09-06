7e3d6d7d71a544be8376234e5c98c1b723b59b85
Ethan <bigeskelly@gmail.com>
2022-09-05 23:34:12 -0600

    Adding Breast Cancer and Glass Data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from Iris import Iris
 from SoyBean import SoyBean
+from BreastCancer import BreastCancer
+from Glass import Glass
 
 
 # Press the green button in the gutter to run the script.
@@ -9,6 +11,10 @@ if __name__ == '__main__':
     I.df.to_csv("iris_output.csv")
     S = SoyBean()
     S.df.to_csv("soybean-small_output.csv")
+    B = BreastCancer()
+    B.df.to_csv('breast-cancer-wisconsin_output.csv')
+    G = Glass()
+    G.df.to_csv('glass_output.csv')
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
