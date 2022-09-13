984613040bdc1e52fda21604403644ced86fafd2
Ian H. Kessler <ianholm9@gmail.com>
2022-09-12 20:00:11 -0600

    Discretized Iris and Added ZeroOneLoss.

--- Iris.py
+++ Iris.py
@@ -4,7 +4,7 @@ class Iris (IanClass):
     def __init__(self):
         features = ['Sepal Length (cm)', 'Sepal Width (cm)', 'Petal Length (cm)',
                          'Petal Width (cm)']
-        IanClass.__init__(self, file = 'iris.csv', features = features, name = "Iris")
+        IanClass.__init__(self, file = 'iris.csv', features = features, name = "Iris", discretize=True)
 
 
 
