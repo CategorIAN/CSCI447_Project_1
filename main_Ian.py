e9e2b00d15d3c5606935edc7825827ab3c1aac46
Ethan10Real <ethan@10Real.com>
2022-09-16 08:25:51 -0600

    adding actual tuning

--- main_Ian.py
+++ main_Ian.py
@@ -5,24 +5,27 @@ from Glass import Glass
 from Vote import Vote
 import pandas as pd
 
-def merge(i,s,c,g,v):
-    print(i.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean'))
-    s.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
-    c.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
-    g.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
-    v.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
-    
-    # merge_df = pd.concat(list(dfs)).reset_index(drop=True)
-    # merge_df.to_csv("merged.csv")
-    # summary_df = merge_df.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
-    # summary_df.to_csv("summary.csv")
+def merge(*dfs):    
+    merge_df = pd.concat(list(dfs)).reset_index(drop=True)
+    merge_df.to_csv("merged.csv")
+    summary_df = merge_df.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
+    summary_df.to_csv("summary.csv")
     # print(summary_df.loc[summary_df['Average'] == summary_df['Average'].max()])
+
+    tuningdf = summary_df.loc[summary_df['Average'] == summary_df['Average'].max()]
+    tuningdf.to_csv("tuningdf.csv")
+                    
 
 
 
 if __name__ == '__main__':
-    starting_bin = 3
-    tuning = 2
+    tuningdf = pd.DataFrame
+    starting_bin = 1
+    tuning = 3
+    if(tuningdf['Average'].iloc[0] != None):
+        print(tuningdf['Bin_Number'].iloc[0])
+        print(tuningdf['M_Value'].iloc[0])
+
     I = Iris()
     I.test(tuning, starting_bin)
     S = SB()
