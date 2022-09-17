b10c8e37ec528d54d64bc73f4cad999b1e3d5ab4
Ethan10Real <ethan@10Real.com>
2022-09-16 21:25:31 -0600

    finishing

--- VideoScripts.py
+++ VideoScripts.py
@@ -9,24 +9,30 @@ from ConfusionMatrix import ConfusionMatrix
 
 
 def show_bins(data, bin_numbers):
-    pd.set_option('max_columns', None)
     print("Original Data:")
+    #printing original dataset
     print(data.df.head())
     print("=================================")
+    #for each of the bins prints out the corresponding dataset
     for bin_number in bin_numbers:
         binned_df = data.bin(data.df, bin_number)
         print("Binned Data with {} Bin(s):".format(bin_number))
         print(binned_df.head())
         print("---------------------------------")
 
+
+#training data
 def show_trained_model(data, bin_number, m_value, partition_index):
-    pd.set_option('max_columns', None)
+    #create bin df
     binned_df = data.bin(data.df, bin_number)
+    #training each partition of the binned data
     data.training_test_sets(partition_index, binned_df, data.partition(10))
+    #class probability
     Qtrain = data.getQ()
     print("Q Frame (Class Parameter Values):")
     print(Qtrain)
     print("=======================================")
+    #for each feature we print feature probability based on bin number and class
     Ftrains = data.getFs(m_value, 1/(100*(m_value)), Qtrain)
     for i in range(len(Ftrains)):
         print("F Frame {}".format(i))
@@ -34,14 +40,20 @@ def show_trained_model(data, bin_number, m_value, partition_index):
         print("------------------------------")
 
 def show_model_count(data, bin_number, m_value, partition_index):
+    #bin dataframe
     binned_df = data.bin(data.df, bin_number)
+    #training each partition of the binned data
     data.training_test_sets(partition_index, binned_df, data.partition(10))
+    #class probability
     Qtrain = data.getQ()
+    #feature probabilities for each feature 
     Ftrains = data.getFs(m_value, 1 / (100 * (m_value)), Qtrain)
     for cl in data.classes:
+        #print out class count
         print("There are {} values for class {}".format(Qtrain.at[cl, 'Count'], cl))
         print("----------------------------------------------------------------------")
         for i in range(len(data.features)):
+            #print out the count of the feature in a certain class with a specfic bin number
             print("Feature: {}".format(data.features[i]))
             print("+++++++++++++")
             for bin in range(1, bin_number + 1):
@@ -51,55 +63,64 @@ def show_model_count(data, bin_number, m_value, partition_index):
         print("==================================================")
 
 def preds_and_evals(data, bin_number, m_value, partition_index):
-    pd.set_option('max_columns', None)
+    #bin dataframe
     binned_df = data.bin(data.df, bin_number)
+    #training each partition of the binned data
     data.training_test_sets(partition_index, binned_df, data.partition(10))
+    #class probability
     Qtrain = data.getQ()
+    #feature probabilities for each feature 
     Ftrains = data.getFs(m_value, 1 / (100 * (m_value)), Qtrain)
     predicted_classes = []
     zero_one_sum = 0
-    CM = ConfusionMatrix(data.classes)
+    CM = ConfusionMatrix(data.classes) #confusion matric for p macro
     result_data = pd.DataFrame(data.test_set.to_dict())
     p = data.partition(10)
+    #print only one fold of the partitions
     for i in p[partition_index]:
         predicted = data.predicted_class(data.value(binned_df, i), Qtrain,
                                          Ftrains)  # predicted class value associated with the row
         actual = binned_df.at[i, 'Class']  # the class value associated with the row
         predicted_classes.append(predicted)
-        zero_one_sum += data.zero_one_loss(predicted, actual)
+        zero_one_sum += data.zero_one_loss(predicted, actual) 
         CM.addOne(predicted, actual)
-    result_data["Predicted_Class"] = predicted_classes
+    result_data["Predicted_Class"] = predicted_classes #create predicted classes column
     print("Test Set with Predictions:")
-    print(result_data)
-    zero_one_avg = zero_one_sum / len(p[partition_index])
+    print(result_data) #print out df
+    zero_one_avg = zero_one_sum / len(p[partition_index]) 
     print("Zero One Loss Average Value: {}".format(zero_one_avg))
     print("P Macro of Confusion Matrix: {}".format(CM.pmacro()))
     print("----------------------------------")
 
-
+    #same thing for the noisy dataset
     print("Now with Noisy Data:")
     noisy_data = data.getNoise()
     binned_df = data.bin(noisy_data, bin_number)
+    #training each partition of the binned data
     data.training_test_sets(partition_index, binned_df, data.partition(10))
+    #class probability
     Qtrain = data.getQ()
+    #feature probabilities for each feature 
     Ftrains = data.getFs(m_value, 1 / (100 * (m_value)), Qtrain)
     predicted_classes = []
     zero_one_sum = 0
-    CM = ConfusionMatrix(data.classes)
+    CM = ConfusionMatrix(data.classes) #confusion matric for p macro
     result_data = pd.DataFrame(data.test_set.to_dict())
     p = data.partition(10)
+    #print only one fold of the partitions
     for i in p[partition_index]:
         predicted = data.predicted_class(data.value(binned_df, i), Qtrain,
                                          Ftrains)  # predicted class value associated with the row
         actual = binned_df.at[i, 'Class']  # the class value associated with the row
         predicted_classes.append(predicted)
-        zero_one_sum += data.zero_one_loss(predicted, actual)
+        zero_one_sum += data.zero_one_loss(predicted, actual) 
         CM.addOne(predicted, actual)
-    result_data["Predicted_Class"] = predicted_classes
+    result_data["Predicted_Class"] = predicted_classes #create predicted classes column
     print("Test Set with Predictions:")
-    print(result_data)
-    zero_one_avg = zero_one_sum / len(p[partition_index])
+    print(result_data) #print out df
+    zero_one_avg = zero_one_sum / len(p[partition_index]) 
     print("Zero One Loss Average Value: {}".format(zero_one_avg))
     print("P Macro of Confusion Matrix: {}".format(CM.pmacro()))
+    print("----------------------------------")
 
 
--- main.py
+++ main.py
@@ -5,41 +5,31 @@ from Glass import Glass
 from Vote import Vote
 import pandas as pd
 from os.path import exists
+import VideoScripts
 
-#merge all the analysis datasets into one and find the best combination for hyperparameters
 def merge(*dfs):    
-    #merge
     merge_df = pd.concat(list(dfs)).reset_index(drop=True)
     merge_df.to_csv("merged.csv")
-
-    #create summary dataframe that avarges each of the combinations and finds the avarges for each combination
     summary_df = merge_df.groupby(by=['Bin_Number', 'M_Value', 'Prob_Value'])[['Average']].agg('mean')
     summary_df.to_csv("summary.csv")
+    # print(summary_df.loc[summary_df['Average'] == summary_df['Average'].max()])
 
-    #find the best combination of hyperparameters
     tuningdf = summary_df.loc[summary_df['Average'] == summary_df['Average'].max()]
     tuningdf.to_csv("tuningdf.csv")
                     
-    # merge_df.to_latex("merge_latex.txt")
-    # tuningdf.to_latex("tuning_latex.txt")
-    summary_df.to_latex("summary_latex.txt")
+    merge_df.to_latex("merge_latex.txt")
+    tuningdf.to_latex("tuning_latex.txt")
 
-
-if __name__ == '__main__':
-    
+def ethanScript():
     tuning = 10
-    #if there is a tuningdf then some tuning has already been done so start from the found hyperparameters
-    if(exists("tuningdf.csv")):
-        tuningdf = pd.read_csv("tuningdf.csv")       #read csv
-        starting_bin = tuningdf['Bin_Number'].iloc[0]   #best bin number
-        m_val = tuningdf['M_Value'].iloc[0] #best m_val
-
-    #otherwise have starting values
+    if (exists("tuningdf.csv")):
+        tuningdf = pd.read_csv("tuningdf.csv")
+        starting_bin = tuningdf['Bin_Number'].iloc[0]
+        m_val = tuningdf['M_Value'].iloc[0]
     else:
         starting_bin = 1
         m_val = 1
-    
-    #got through and test each data set
+
     I = Iris()
     I.test(tuning, starting_bin, m_val)
     S = SB()
@@ -50,9 +40,24 @@ if __name__ == '__main__':
     G.test(tuning, starting_bin, m_val)
     V = Vote()
     V.test(tuning, starting_bin, m_val)
-
-    #merge the analysis dataframes
     merge(I.analysis_df, S.analysis_df, C.analysis_df, G.analysis_df, V.analysis_df)
+    I.analysis_df.to_latex("i_analysis_latex.txt")
+
+# def random():
     
-    # I.analysis_df.to_latex("i_analysis_latex.txt")
+
+
+
+if __name__ == '__main__':
+    D = Iris()
+    VideoScripts.show_bins(data=D, bin_numbers=list(range(1, 4)))
+    VideoScripts.show_trained_model(D, 11, 1, 0)
+    VideoScripts.show_model_count(D, 11, 1, 0)
+    VideoScripts.preds_and_evals(D, 11, 1, 6)
+
+
+
+
+    
+
 
